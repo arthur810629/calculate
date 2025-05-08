1c513fe6d2edde037cf2dac425cd6ca0f38a1745
Артур Бутюгов <arthur8106@mail.ru>
2025-05-08 22:17:45 +0300

    update 0.2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,21 @@
-// src/components/Header.jsx
 import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
-import {
-  FaUniversity,
-  FaPiggyBank,
-  FaCalculator,
-  FaCar,
-  FaCalendarDay,
-  FaFileInvoice
-} from 'react-icons/fa'
+import { FaThLarge } from 'react-icons/fa'
 import './Header.css'
 
 export default function Header() {
-  const [menuOpen, setMenuOpen] = useState(false)
   const [megaOpen, setMegaOpen] = useState(false)
 
   return (
     <header className="site-header">
       <div className="container header-inner">
         <div className="logo">
-          <NavLink to="/">ReactCalculators</NavLink>
+          <NavLink to="/" aria-label="На главную">
+            ReactCalculators
+          </NavLink>
         </div>
 
         <nav className="site-nav" aria-label="Основное меню">
-          <NavLink to="/" className="nav-link">Главная</NavLink>
-
-          {/* Пункт с мегадропдауном */}
           <div
             className="nav-item mega-parent"
             onMouseEnter={() => setMegaOpen(true)}
@@ -36,51 +26,49 @@ export default function Header() {
               aria-haspopup="true"
               aria-expanded={megaOpen}
             >
-              Финансовые ▾
+              <FaThLarge size={16} />
+              <span>Калькуляторы</span>
             </button>
 
             <div className={`mega-menu ${megaOpen ? 'open' : ''}`} role="menu">
-              {/* Колонка 1 */}
+              {/* Финансовые калькуляторы */}
               <div className="mega-section">
-                <h4><FaUniversity /> Кредиты</h4>
+                <h4>Финансы</h4>
+                <NavLink to="/category/finance" className="mega-link">Все финансы</NavLink>
                 <NavLink to="/calculator/finance/credit" className="mega-link">Кредитный</NavLink>
                 <NavLink to="/calculator/finance/mortgage" className="mega-link">Ипотечный</NavLink>
-                <NavLink to="/calculator/finance/prepayment" className="mega-link">С досрочным погашением</NavLink>
+                <NavLink to="/calculator/finance/prepayment" className="mega-link">Досрочное погашение</NavLink>
               </div>
-              {/* Колонка 2 */}
+
+              {/* Инвестиции */}
               <div className="mega-section">
-                <h4><FaPiggyBank /> Инвестиции</h4>
-                <NavLink to="/calculator/finance/deposit" className="mega-link">Вклады</NavLink>
-                <NavLink to="/calculator/finance/investment" className="mega-link">Инвестиции</NavLink>
-                <NavLink to="/calculator/finance/bonds" className="mega-link">Облигации</NavLink>
+                <h4>Инвестиции</h4>
+                <NavLink to="/category/invest" className="mega-link">Все инвестиции</NavLink>
+                <NavLink to="/calculator/invest/deposit" className="mega-link">Вклады</NavLink>
+                <NavLink to="/calculator/invest/investment" className="mega-link">Инвестиции</NavLink>
+                <NavLink to="/calculator/invest/bonds" className="mega-link">Облигации</NavLink>
               </div>
-              {/* Колонка 3 */}
+
+              {/* Бухгалтерия и налоги */}
               <div className="mega-section">
-                <h4><FaCalculator /> Налоги</h4>
-                <NavLink to="/calculator/finance/nds" className="mega-link">НДС</NavLink>
-                <NavLink to="/calculator/finance/pit" className="mega-link">НДФЛ</NavLink>
-                <NavLink to="/calculator/finance/penalties" className="mega-link">Пени</NavLink>
+                <h4>Налоги</h4>
+                <NavLink to="/category/accounting" className="mega-link">Все налоги</NavLink>
+                <NavLink to="/calculator/accounting/nds" className="mega-link">НДС</NavLink>
+                <NavLink to="/calculator/accounting/pit" className="mega-link">НДФЛ</NavLink>
+                <NavLink to="/calculator/accounting/penalties" className="mega-link">Пени</NavLink>
               </div>
-              {/* Колонка 4 */}
+
+              {/* Автомобильные */}
               <div className="mega-section">
-                <h4><FaCalendarDay /> Прочее</h4>
-                <NavLink to="/calculator/finance/inflation" className="mega-link">Инфляция</NavLink>
-                <NavLink to="/calculator/finance/refinance" className="mega-link">Рефинансирование</NavLink>
+                <h4>Авто</h4>
+                <NavLink to="/category/auto" className="mega-link">Все авто</NavLink>
+                <NavLink to="/calculator/auto/osago" className="mega-link">ОСАГО</NavLink>
+                <NavLink to="/calculator/auto/fuel" className="mega-link">Расход топлива</NavLink>
+                <NavLink to="/calculator/auto/autocredit" className="mega-link">Автокредит</NavLink>
               </div>
             </div>
           </div>
-
-          <NavLink to="/category/invest" className="nav-link">Инвестиции</NavLink>
-          <NavLink to="/category/tax" className="nav-link">Налоги</NavLink>
-          <NavLink to="/category/auto" className="nav-link">Авто</NavLink>
         </nav>
-
-        <button
-          className="menu-toggle"
-          aria-label={menuOpen ? 'Закрыть меню' : 'Открыть меню'}
-          aria-expanded={menuOpen}
-          onClick={() => setMenuOpen(o => !o)}
-        >☰</button>
       </div>
     </header>
   )
