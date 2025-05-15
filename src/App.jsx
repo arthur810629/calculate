5823d2e0ecdf89f93f7cd59ab2717a6b7b9c05ed
Артур Бутюгов <arthur8106@mail.ru>
2025-05-15 23:18:45 +0300

    yandex

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
 // src/App.jsx
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Header         from './components/Header'
-import Footer         from './components/Footer'
-import Home           from './pages/Home'
-import CategoryPage   from './pages/CategoryPage'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import AdBanner from './components/AdBanner'
+import Home from './pages/Home'
+import CategoryPage from './pages/CategoryPage'
 import CalculatorPage from './pages/CalculatorPage'
 
 export default function App() {
   return (
     <>
+      {/* Реклама сразу под тегом <body>, перед шапкой */}
+      <AdBanner blockId="R-A-15506473-1" />
+
       <Header />
+
       <main className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
-
-          {/* Страница со списком калькуляторов категории */}
           <Route path="/category/:categoryId" element={<CategoryPage />} />
-
-          {/* Конкретный калькулятор */}
           <Route path="/calculator/:categoryId/:calculatorId" element={<CalculatorPage />} />
-
-          {/* На всякий случай: если ни один маршрут не подошёл */}
-          <Route path="*" element={
-            <div className="container">
-              <h1>Страница не найдена</h1>
-            </div>
-          } />
+          <Route
+            path="*"
+            element={
+              <div className="container">
+                <h1>Страница не найдена</h1>
+              </div>
+            }
+          />
         </Routes>
       </main>
+
+      {/* Реклама перед подвалом */}
+      <AdBanner blockId="R-A-15506473-1" />
+
       <Footer />
     </>
   )
